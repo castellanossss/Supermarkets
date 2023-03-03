7691026317c156964f95463c252461a24ba10aa5
Leonardo Castellanos <jaider.castellanos@uptc.edu.co>
2023-03-02 20:34:28 -0500

    Se añadieron metodos para controlar los datos ingresados por el usuario en cada uno de los menús disponibles

--- src/presenter/Presenter.java
+++ src/presenter/Presenter.java
@@ -1,12 +1,110 @@
 package presenter;
 
+import view.View;
+
 public class Presenter {
+	private View view;
 	
+	public Presenter() {
+		view = new View();
+	}
+	
+	public void newSupplierOption() {
+		
+		view.displayArchiveInfo();
+	}
+	
+	public void newCustomerOption() {
+		
+		view.displayArchiveInfo();
+	}
+	
+	public void newProductOption() {
+		
+		view.displayArchiveInfo();
+	}
+	
+	public void recordSaleOption() {
+		
+		view.displayArchiveInfo();
+	}
+	
+	public void showAllSalesOption() {
+		
+		view.displayArchiveInfo();
+	}
+	
+	
+	private void firstDecision() {
+		int option = view.firstMenu();
+		
+		switch (option) {
+			case 1:
+				secondDecision();
+			case 2:
+				view.goodbyeMessage();
+				System.exit(0);
+				break;
+			default:
+				view.alertMessage();
+				firstDecision();
+		}
+	}
+	
+	private void secondDecision() {
+		int option = view.secondMenu();
+		
+		switch (option) {
+			case 1:
+				newSupplierOption();
+				thirdDecision();
+			case 2:
+				newCustomerOption();
+				thirdDecision();
+			case 3:
+				newProductOption();
+				thirdDecision();
+			case 4:
+				recordSaleOption();
+				thirdDecision();
+			case 5:
+				showAllSalesOption();
+				thirdDecision();
+			case 6:
+				view.goodbyeMessage();
+				System.exit(0);
+				break;
+			default:
+				view.alertMessage();
+				secondDecision();
+		}
+	}
+	
+	private void thirdDecision() {
+		int option = view.thirdMenu();
+		
+		switch (option) {
+			case 1:
+				secondDecision();
+			case 2:
+				view.goodbyeMessage();
+				System.exit(0);
+				break;
+			default:
+				view.alertMessage();
+				thirdDecision();
+		}
+	}
+	
+	private void run() {
+		view.welcomeMessage();
+		firstDecision();
+	}
 	
 
 	public static void main(String[] args) {
-		
-
+		Presenter main = new Presenter();
+		main.run();
 	}
 
 }

343bde62addc60bc2f449c8d7f9f5cf3e6920a56
Leonardo Castellanos <jaider.castellanos@uptc.edu.co>
2023-03-03 12:43:08 -0500

    Anexación métodos para la lógica de la aplicacion

--- src/presenter/Presenter.java
+++ src/presenter/Presenter.java
@@ -1,5 +1,15 @@
 package presenter;
 
+import java.text.NumberFormat;
+import java.time.LocalDate;
+
+import model.Address;
+import model.CategoriesManager;
+import model.CustomerManager;
+import model.ProductsManager;
+import model.SalesManager;
+import model.SuppliersManager;
+import tools.FileManagerReader2;
 import view.View;
 
 public class Presenter {
@@ -10,27 +20,69 @@ public class Presenter {
 	}
 	
 	public void newSupplierOption() {
-		
+		supplier = new SuppliersManager("Files\\", "suppliers");
+		supplier.createSupplier(view.askForSupRut(), view.askForSupName(), 
+				new Address(view.askForSupCountry(), view.askForSupState(), view.askForSupCity(), view.askForSupNeighbordhood(), view.askForSupAddress()), 
+				view.askForSupPhoneNumber(), view.askForSupWebPage());
 		view.displayArchiveInfo();
 	}
 	
 	public void newCustomerOption() {
+		customer = new CustomerManager("Files\\", "customers");
+		customer.createCustomer(view.askForCustomerRut(), view.askForCustomerName(),
+				new Address(view.askForCustomerCountry(), view.askForCustomerState(), view.askForCustomerCity(), view.askForCustomerNeighbordhood(), view.askForCusAddress()));
 		
+		int j = view.askForPhoneNumberAmount();
+		for (int i = 1; i <= j; i++) {
+			customer.addPhone(view.askForCusPhoneNumber(i));
+		}
+		customer.registerCustomer(customer.getCustomer());
 		view.displayArchiveInfo();
 	}
 	
 	public void newProductOption() {
-		
+		categories = new CategoriesManager("Files\\","categories");
+		product = new ProductsManager("Files\\","products");
+		categories.readCategories();
+		product.createProduct(12, "brayan", 12, 3, "camilo", categories);
 		view.displayArchiveInfo();
 	}
 	
-	public void recordSaleOption() {
+	public LocalDate generate_bill_date() {
+		int sale_day = view.askForSaleDay();
+		int sale_month = view.askForSaleMonth();
+		int sale_year = view.askForSaleYear();
 		
+		LocalDate date = LocalDate.of(sale_year, sale_month, sale_day);
+		return date;
+	}
+	
+	public void recordSaleOption() {
+		int id = view.askForSaleId();
+		view.salesDateInfo();
+		LocalDate date = generate_bill_date();
+		String customerName = view.askForSaleCustomer();
+		long disc = view.askForSaleDiscount();
+		float fp = view.askForSaleFinalBill();
+		
+		NumberFormat format = NumberFormat.getCurrencyInstance();
+		String discount = format.format(disc);
+		String finalPrice = format.format(fp);
+		
+		sale = new SalesManager("Files\\", "bills");
+		sale.createSale(id, date, customerName, discount, finalPrice);
 		view.displayArchiveInfo();
 	}
 	
 	public void showAllSalesOption() {
+		String path = "Files\\bills.txt";
+		test = new FileManagerReader2(path);
+		String result = test.readFile();
 		
+		view.showMessage("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n VENTAS REGISTRADAS\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+				+ " La informacion esta organizada de la siguiente forma\n"
+				+ " id  ~  Fecha  ~  Nombre del Proveedor  ~  Valor del Descuento  ~  Valor Final\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" 
+				+ result);
 		view.displayArchiveInfo();
 	}
 	
